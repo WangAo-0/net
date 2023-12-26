b8efbf1c1dedf8cb64c9466d0ed3fea85e23f4ef
oliverdebox@163.com <oliver@LAPTOP-Q7N2E2ED>
2023-12-26 21:35:42 +0800

    init
    Signed-off-by: oliverdebox@163.com <oliver@LAPTOP-Q7N2E2ED>

---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Description: 测试
+ * @Author: oliver
+ * @version: 1.0
+ * @Date: 2023-12-26 20:50:59
+ * @LastEditors: oliver
+ * @LastEditTime: 2023-12-26 21:10:43
+ */
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello world"<<endl;
+    return 0;
+}
